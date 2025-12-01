ce05cd3e14ea59c75606e52efc4ad58f4b16686d
Andres-2707 <andresjvj652@gmail.com>
2025-12-01 14:52:16 -0600

    CP2

---
+++ CP2/CustomQueue.java
@@ -0,0 +1,57 @@
+public class CustomQueue {
+
+    private QueueNode _head;
+
+    public CustomQueue() {
+        _head = null;
+    }
+
+    public void enqueue(int index) {
+        var node = new QueueNode(index);
+
+        // Actualizar
+        //ChatGPT 5.1
+        if (_head == null) {
+    _head = node;
+} else {
+    var curr = _head;
+    while (curr.getNext() != null) {
+        curr = curr.getNext();
+    }
+    curr.setNext(node);
+}
+    }
+
+    public int dequeue() {
+        var index = -1;
+
+        // Actualizar
+        if (_head != null) {
+    index = _head.getIndex();
+    _head = _head.getNext();
+}
+
+        return index;
+    }
+
+    public int getSize() {
+        var size = 0;
+
+        // Actualizar
+        var curr = _head;
+while (curr != null) {
+    size++;
+    curr = curr.getNext();
+}
+
+        return size;
+    }
+
+    public String getIndexes() {
+        var builder = new StringBuilder();
+        for (var index = dequeue(); index > -1; index = dequeue()) {
+            builder.append(" " + index);
+        }
+        return builder.toString();
+    }
+}
